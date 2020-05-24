40531845bc054e621d08da0179c6163ec8e579f2
Austin Tao <60298040+alt2177@users.noreply.github.com>
2020-05-24 15:36:06 -0700

    added data

--- HealthHack/HealthHack/api/change.py
+++ HealthHack/HealthHack/api/change.py
@@ -3,29 +3,15 @@ from django.http import HttpResponse, HttpResponseNotFound
 from ..models import company, log
 import django
 
-from django.views.decorators.csrf import csrf_exempt
-
-# expected method is POST
-
-@csrf_exempt
 def change(request):
     if request.method == "POST":
-        # checks if a company exists with the passed in api key
-        if company.objects.filter(key = request.POST['key']).count() == 1:
-            # updates the population for that company
-            currentCompany = company.objects.get(key = request.POST['key'])
-            currentCompany.currentPopulation = int(request.POST['population'])
+        if company.objects.count(key = request['key']) == 1:
+            currentCompany = company.objects.get(key = request['key'])
+            currentCompany.currentPopulation = int(request['population'])
             currentCompany.save()
-
-            # creates a new log object to store the passed in data
-            log.objects.create(companyId = currentCompany, population = request.POST['population'],
-            date = request.POST['date'], time = request.POST['time'])
-
-            # returns success code
-            return HttpResponse(status = 200)
+            log.objects.create(companyID = currentCompany, population = request['population'],
+            date = request['date'], time = request['time'])
         else:
-            # invalid data code
             return HttpResponse(status = 401)
     else:
-        # invalid request type
 	    return HttpResponse(status = 404)
--- HealthHack/HealthHack/views/company.py
+++ HealthHack/HealthHack/views/company.py
@@ -8,17 +8,12 @@ import datetime
 
 
 
-# shows the company specific page
-# expecting get request with the company
+
 def company(request):
     if request.method == "GET":
         context = loadData()
-        # checks to see if an id has been passed in
         if "company" in request.GET:
-            # get the current company and store it in the context
             context["company"] = companyDB.objects.get(id = int(request.GET["company"]))
-
-            # get the data in the correct order
             filteredData = dailyAverage.objects.filter(companyID = context["company"])
             hourMapper = []
             for i in range(1,24):
@@ -28,8 +23,6 @@ def company(request):
 
             for time in hourMapper:
                 out.append(filteredData.get(time = time).population)
-
-            # pass in the hourly data to be used for the chart
             context["averages"] = out
 
 
